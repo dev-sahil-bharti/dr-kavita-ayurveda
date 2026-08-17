3e5e3b41413f76b8d927792ec8d6e548e4af551b
er-sahilbharti <sahil.884062@gmail.com>
2026-08-17 12:59:51 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 // index.js
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-const { startReminderCron } = require('./src/cron/reminderCron');
+const { startReminderCron } = require("./src/cron/reminderCron");
 
+// Connect Database
 connectDB();
+
+// Start reminder cron
 startReminderCron();
 
+// Render provides PORT automatically
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
